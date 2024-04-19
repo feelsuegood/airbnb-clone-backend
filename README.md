ab3d492b52d1f68a515cd8628fe49bd9c4f5d4d6
feelsuegood <suaj2021@gmail.com>
2024-04-20 05:12:19 +1000

    update

---
+++ README.md
@@ -0,0 +1,58 @@
+poetry init
+poetry add django -> create poetry.lock
+poetry shell
+
+django-admin
+django-admin startproject config .
+
+gitignore extension -> open palette -> gitignore -> python -> create
+setup.md -> into .gitignore
+
+python manage.py runserver
+ctrl+c
+python manage.py migrate
+python manage.py createsuperuser
+
+python manage.py startapp houses
+
+- migration for houses
+  python manage.py makemigrations
+  python manage.py migrate
+
+poetry add pillow
+
+# Django documentation link: https://docs.djangoproject.com/en/5.0/
+
+Must replace user model with custom user model
+
+python managy.py startapp users -> Must do this from the beginning, when there is no user.
+
+- in the mid-project, delete database, "001_initial.py, 002_initial.py"
+- Don't delete migrations folder and **init**.py
+
+# migration: sync database structure with model structure in python code (models.py)
+
+# VScode extensin
+
+- SQLite viewr
+- Black fomatter
+
+If you want to set up an interpreter in poetry, you can type command "poetry env use python"
+then put that location in the interpreter pallette.
+
+[tool.poetry]
+name = "airbnb-clone"
+version = "0.1.0"
+description = ""
+authors = ["feelsuegood <suaj2021@gmail.com>"]
+license = "MIT"
+readme = "README.md"
+
+[tool.poetry.dependencies]
+python = "^3.12"
+django = "^5.0.4"
+pillow = "^10.3.0"
+
+[build-system]
+requires = ["poetry-core"]
+build-backend = "poetry.core.masonry.api"
---
+++ rooms/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from common.models import CommonModel
+
+
+class Room(CommonModel):
+    """Room Model Definition"""
+
+    class RoomKindChoices(models.TextChoices):
+        ENTIRE_PLACE = ("entire_place", "Entire Place")
+        PRIVAE_ROOM = ("private_room", "Private Room")
+        SHARE_ROOM = ("share_room", "Share Room")
+
+    name = models.CharField(
+        max_length=180,
+        default="",
+    )
+    country = models.CharField(
+        max_length=50,
+        default="Korea",
+    )
+    city = models.CharField(
+        max_length=80,
+        default="Seoul",
+    )
+    price = models.PositiveIntegerField()
+    rooms = models.PositiveIntegerField()
+    toilets = models.PositiveIntegerField()
+    description = models.TextField()
+    address = models.CharField(
+        max_length=250,
+    )
+    pet_friendly = models.BooleanField(
+        default=True,
+    )
+    kind = models.CharField(
+        max_length=20,
+        choices=RoomKindChoices.choices,
+    )
+    owner = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
+    amenities = models.ManyToManyField(
+        "rooms.Amenity",
+        null=True,
+        blank=True,
+    )
+    category = models.ForeignKey(
+        "categories.Category",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Amenity(CommonModel):
+    """Amenity Definition"""
+
+    name = models.CharField(max_length=150)
+    description = models.CharField(
+        max_length=150,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Amenities"
