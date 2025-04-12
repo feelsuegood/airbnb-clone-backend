1e3221682bb99d6e0b14e03f7a08adf911899e65
feelsuegood <suaj2021@gmail.com>
2025-04-12 21:20:19 +1000

    9.6 django template

--- rooms/views.py
+++ rooms/views.py
@@ -2,12 +2,38 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 
 # Create your views here.
 def see_all_rooms(request):
-    return HttpResponse("see all rooms")
+    rooms = Room.objects.all()
+    return render(
+        request,
+        "all_rooms.html",
+        {
+            "rooms": rooms,
+            "title": "This title is from django",
+        },
+    )
 
 
-def see_one_rooms(request, room_id):
-    return HttpResponse(f"see a room with id: {room_id}")
+def see_one_rooms(request, room_pk):
+    try:
+        room = Room.objects.get(pk=room_pk)
+        # it's hard to make a dynamic website
+        return render(
+            request,
+            "room_detail.html",
+            {
+                "room": room,
+            },
+        )
+    except Room.DoesNotExist:
+        return render(
+            request,
+            "room_detail.html",
+            {
+                "not_found": True,
+            },
+        )
