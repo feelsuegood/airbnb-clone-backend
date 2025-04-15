e2af27477ba9aead695815f58cff603def7a1aac
feelsuegood <suaj2021@gmail.com>
2025-04-15 11:53:41 +1000

    10.13 ModelViewSet - too magical (inplicit)

--- categories/urls.py
+++ categories/urls.py
@@ -1,7 +1,25 @@
+from venv import create
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.Categories.as_view()),
-    path("<int:pk>", views.CategoryDetail.as_view()),
+    path(
+        "",
+        views.CategoryViewSet.as_view(
+            {
+                "get": "list",
+                "post": "create",
+            }
+        ),
+    ),
+    path(
+        "<int:pk>",
+        views.CategoryViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+    ),
 ]
