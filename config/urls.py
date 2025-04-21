75478abacb008e1560ce83e6c518d87a03dbf067
feelsuegood <suaj2021@gmail.com>
2025-04-22 08:52:14 +1000

    14.4 strawberry graphql practice

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from strawberry.django.views import GraphQLView
 from . import settings
+from .schema import schema
 
 
 urlpatterns = [
@@ -29,4 +31,5 @@ urlpatterns = [
     path("api/v1/medias/", include("medias.urls")),
     path("api/v1/wishlists/", include("wishlists.urls")),
     path("api/v1/users/", include("users.urls")),
+    path("graphql", GraphQLView.as_view(schema=schema)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
